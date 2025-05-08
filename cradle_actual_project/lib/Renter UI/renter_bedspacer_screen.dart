216243f8cdb8e5f6dacc01cb28a7aa855065b88e
144px <149484079+144px@users.noreply.github.com>
2025-05-08 08:45:25 +0800

    Update renter_bedspacer_screen.dart
    Edited the UI to align with Figma

--- cradle_actual_project/lib/Renter UI/renter_bedspacer_screen.dart
+++ cradle_actual_project/lib/Renter UI/renter_bedspacer_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../Test/bedspace.dart'; // This imports for_rent.dart implicitly
+import '../Test/bedspace.dart'; 
 import 'package:logger/logger.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../utils/string_extensions.dart';
